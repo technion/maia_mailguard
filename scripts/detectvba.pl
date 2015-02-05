803a51956ece43a84cba168deef4e2c45483e7f0
Joshua Small <jsmall@daraco.com.au>
2015-02-05 15:38:10 +1100

    Added VBA macro detection

---
+++ scripts/detectvba.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+#technion@lolware.net
+#Detects vba macros containing blacklisted strings.
+#
+#Suggested maiad.conf config:
+#['Detect-VBA',
+#   '/var/maia/scripts/detectvba.pl', "{}",
+#      [0], qr/INFECTED/, qr/\bINFECTED (.+)\b/m ],
+#
+use strict;
+
+my $sigtool = '/usr/local/bin/sigtool'; #Clamav sigtool path
+
+if ($#ARGV != 0) {
+    print "Please supply directory to scan\n";
+    exit 0;
+}
+
+#Sanity check directory
+my $dir = $ARGV[0];
+if ($dir !~ /^[a-z0-9A-Z\/-]+$/) {
+    print "Invalid directory passed\n";
+    exit 0;
+}
+
+opendir DIR, $dir or die "Cannot open dir $dir: $!";
+my @files = readdir DIR;
+
+foreach my $file (@files) {
+    my $scan = `$sigtool --vba="$dir/$file"`;
+    if ($scan =~ /AppData\\Local\\Temp/i ) {
+        print "Scanning $file: INFECTED VBA\n";
+        exit 1;
+    } else {
+        print "Scanning $file: OK\n";
+    }
+}
+
+closedir DIR;
+
+exit 0;
