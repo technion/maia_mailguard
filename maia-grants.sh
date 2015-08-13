36de904fb75ce5cd58e96908efabacad91a7add8
J J Sloan <jjs@mainphrame.com>
2015-08-12 21:57:34 -0700

    installer option testing

--- maia-grants.sh
+++ maia-grants.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# set mysql password
 PASSWORD='password'
+# set mysql password
+if [ $# -eq 1 ]; then
+  PASSWORD=$1
+fi
+
+echo "password = $PASSWORD"
+exit
 
 mysql <<ZZ
 GRANT CREATE, DROP, ALTER, SELECT, INSERT, UPDATE, DELETE ON maia.* TO maia@localhost IDENTIFIED BY "$PASSWORD";
